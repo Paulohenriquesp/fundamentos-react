26c55dfdd01b2b03e14f9a8da29bdba99e24310a
Paulo <paulosouzap7@gmail.com>
2023-10-05 10:03:29 -0300

    feat: useState

--- 2_AVANCANDO_EM_REACT/avancando-react/src/componentes/ManageData.jsx
+++ 2_AVANCANDO_EM_REACT/avancando-react/src/componentes/ManageData.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 export const ManageData = () => {
-  const someData = 10;
+  let someData = 10;
+  
+
+  const [number, setNumber] = useState(10);
 
   return (
     <div>
       <div>
         <p>Valor: {someData}</p>
+        <button onClick={() => (someData = 15)}>Mudar variável</button>
       </div>
+        <p>Valor: {number} </p>
+        <button onClick={() => setNumber(25)}>Mudar valor</button>
     </div>
   );
 };
