c84392feea63c8051af547c7236dc2a8ba81bfa8
Paulo <paulosouzap7@gmail.com>
2023-10-06 09:41:08 -0300

    feat: if e else / ternário

--- 2_AVANCANDO_EM_REACT/avancando-react/src/componentes/ConditionalRender.tsx
+++ 2_AVANCANDO_EM_REACT/avancando-react/src/componentes/ConditionalRender.tsx
@@ -1,13 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const ConditionalRender = () => {
-    const [x] = useState(true);
+  const [x] = useState(true);
+
+  const [name] = useState("Joao");
+
   return (
     <div>
-        <h1>Isso será exibido?</h1>
-        {x && <p>Se x for true, sim!</p>}
-    </div>
-  )
-}
+      <h1>Isso será exibido?</h1>
+      {x && <p>Se x for true, sim!</p>}
 
-export default ConditionalRender
+      {name === "Joao" ? (
+        <div>Meu nome é Joao =D</div>
+      ) : (
+        <div>Meu nome não é Joao</div>
+      )}
+    </div>
+  );
+};
+
+export default ConditionalRender;
