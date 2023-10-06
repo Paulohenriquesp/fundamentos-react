52b36c56f17516d952f869dd0d4e3f6587ce8e36
Paulo <paulosouzap7@gmail.com>
2023-10-06 08:54:27 -0300

    feat: usando propriedade key pegando passando id como key

--- 2_AVANCANDO_EM_REACT/avancando-react/src/componentes/ListRender.tsx
+++ 2_AVANCANDO_EM_REACT/avancando-react/src/componentes/ListRender.tsx
@@ -4,13 +4,24 @@ import {useState} from 'react'
 const ListRender = () => {
 const [list] = useState(["Joao", "Pedro", "Augusto", "Mario"])
 
-
+const [users] = useState([
+    {id: 1, name: "Matheus", age: 31},
+    {id: 2, name: "Pedro", age: 17},
+    {id: 3, name: "Ramon", age: 21}
+])
   return (
   <div>
         <ul>
-            {list.map((item) => (
-                <li>
+            {list.map((item, i) => (
+                <li key={i}>
                     {item}
+                </li>
+            ))}
+        </ul>
+        <ul>
+            {users.map((user) => (
+                <li key={user.id}>
+                    {user.name} - {user.age}
                 </li>
             ))}
         </ul>
